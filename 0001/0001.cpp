30431bbf5984dba342812de7149533dd28d426ba
yanguoqiang <yanguoqiang@yiritech.com>
2024-02-19 15:04:02 +0800

    更新vector初始化方式，用{}

--- 0001/0001.cpp
+++ 0001/0001.cpp
@@ -7,7 +7,7 @@ class Solution {
 public:
 	static std::vector<int> twoSum(std::vector<int>& nums, int target) {
 		std::vector<int> result(2, -1);	//count, init value
-		std::map<int, int> buffer ;
+		std::map<int, int> buffer ;	// key是值，value是所在数组下标
 
 		for (int i = 0; i < nums.size(); i++) {
 			//判断机加是否等于目标
@@ -29,16 +29,13 @@ int main()
 {
 	std::cout << "两数之和" << std::endl;
 
-	std::vector<int> nums(3);
-	nums[0] = 3;
-	nums[1] = 2;
-	nums[2] = 4;
+	std::vector<int> nums = {3,2,4};
 
 	std::vector<int> result = Solution::twoSum(nums, 6);
 
 	for (int it : result)
 	{
-		std::cout << it << std::endl;
+		std::cout << it << " ";
 	}
 	return 0;
 }
