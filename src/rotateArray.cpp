27a39dfc6785b367ad2fefb136162cdafb7d24aa
yanguoqiang <yanguoqiang@yiritech.com>
2024-02-23 19:11:54 +0800

    用取余实现数组循环，注意指针的引用关系

--- src/rotateArray.cpp
+++ src/rotateArray.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <algorithm>
 
+int result[9];
+
 class Solution
 {
 public:
@@ -22,17 +24,38 @@ public:
       return nums;
     } */
 
-  int* rotate(int nums[], int k, int n)
+  int *rotate(int nums[], int k, int n)
   {
+    // int size = sizeof(nums)/sizeof(nums[0]); error：sizeof是在编译阶段展开的一个宏，函数参数在运行阶段以压栈方式传递了一个地址
     k %= n;
     reverse(nums, 0, n - k);
     reverse(nums, n - k, n);
     reverse(nums, 0, n);
-    std::cout<<"nums address "<< nums <<std::endl;
+    std::cout << "nums address " << nums << std::endl;
     return nums;
   }
-  int* test(int arr[]){
+  int *test(int arr[])
+  {
     return arr;
+  }
+
+  int *rotate1(int **nums, int k, int n)
+  {
+    // error 始终是1，实际算的是指针长度
+    //  int size = sizeof(*nums)/sizeof(nums[0]);
+    //  int result[n];
+    for (int i = 0; i < n; i++)
+    {
+      // int v = *((*nums) + i);
+      int v = (*nums)[i];
+      int index = (i + k) % n;
+      result[index] = v;
+    }
+    std::cout << "nums address " << *nums << std::endl;
+    // memcpy(nums, result, n);
+    *nums = result; // nums只是一个临时变量，保存了一个地址；改变了这个临时变量保存的地址，并不会影响外部数组地址
+    std::cout << "nums address " << *nums << std::endl;
+    return *nums;
   }
 
 private:
@@ -60,14 +83,32 @@ int main(int argc, char *argv[])
   }
   Solution s;
   int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int* a = new int[5]{1,2,3,4,5};
+  int *a = new int[5]{1, 2, 3, 4, 5};
   delete a;
-  std::cout<<"arr address "<< arr <<std::endl;
+  std::cout << "arr address " << arr << std::endl;
+  std::cout << "result address " << result << std::endl;
   int n = sizeof(arr) / sizeof(arr[0]);
-  s.rotate(arr, 4, n);
+  // s.rotate(arr, 4, n);
+  a = arr;
+  std::cout << "a " << a << std::endl;
+  int *n1 = s.rotate1(&a, 4, n);
+  std::cout << "arr address " << arr << std::endl;
   for (int i = 0; i < n; i++)
   {
     std::cout << arr[i];
+  }
+  std::cout << std::endl
+            << "n1 " << n1 << std::endl;
+  std::cout << "a " << a << std::endl;
+  for (int i = 0; i < n; i++)
+  {
+    std::cout << *(n1 + i);
+  }
+  std::cout << std::endl
+            << "array " << std::endl;
+  for (int i = 0; i < n; i++)
+  {
+    std::cout << *(arr + i);
   }
   return 0;
 }
